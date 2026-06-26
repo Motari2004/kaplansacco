6c8e1b75f1b42dfb251ec85634de607dd45d6625
Motari2004 <hopefreymosingi1@gmail.com>
2026-06-26 16:03:35 +0300

    Describe what you changed here

---
+++ types/next-auth.d.ts
@@ -0,0 +1,31 @@
+import { DefaultSession } from "next-auth"
+
+declare module "next-auth" {
+  interface Session {
+    user: {
+      id: string
+      role: string
+      memberNumber: string
+      firstName: string
+      lastName: string
+    } & DefaultSession["user"]
+  }
+
+  interface User {
+    id: string
+    role: string
+    memberNumber: string
+    firstName: string
+    lastName: string
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    id: string
+    role: string
+    memberNumber: string
+    firstName: string
+    lastName: string
+  }
+}
