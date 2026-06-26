7f32bdbd26fd28c0d2e04cd34a410363a7442382
Motari2004 <hopefreymosingi1@gmail.com>
2026-06-26 16:32:54 +0300

    Describe what you changed here

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,7 +1,7 @@
-import { PrismaClient } from '@prisma/client/edge'
+import { PrismaClient } from '@prisma/client'
 import { PrismaPg } from '@prisma/adapter-pg'
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient }
+const globalForPrisma = global as unknown as { prisma: InstanceType<typeof PrismaClient> }
 
 if (!process.env.DATABASE_URL) {
   throw new Error('DATABASE_URL environment variable is not set')
